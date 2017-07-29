15cdcdd4b030d0ee29eabf0dc807f3c15ba468a3
fastvepik <andrey.yurin55@gmail.com>
2017-07-29 11:34:01 +0300

    added some imgs

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,6 +7,7 @@ import com.mygdx.game.MyGdxGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		System.setProperty("org.lwjgl.opengl.Display.allowSoftwareOpenGL", "true");
 		new LwjglApplication(new MyGdxGame(), config);
 	}
 }
